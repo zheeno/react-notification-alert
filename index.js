260777f0b45ba4a1fa395240e27c623811aa743f
Efezino Ukpowe <efezinoukpowe@gmail.com>
2020-08-09 17:30:32 +0100

    Update index.js
    Added row

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Alert, Col } from "reactstrap";
+import { Alert, Col, Row } from "reactstrap";
 
 class NotificationAlert extends React.Component {
   constructor(props) {
@@ -18,7 +18,7 @@ class NotificationAlert extends React.Component {
   }
   // to stop the warning of calling setState of unmounted component
   componentWillUnmount() {
-    for(let i = 0; i < this.state.notifyID.length; i++){
+    for (let i = 0; i < this.state.notifyID.length; i++) {
       window.clearTimeout(this.state.notifyID[i]);
     }
   }
@@ -61,7 +61,7 @@ class NotificationAlert extends React.Component {
     }
     if (noAnimate === undefined) {
       let id = setTimeout(
-        function() {
+        function () {
           this.onDismiss(nNumber, place, "noAnimate");
         }.bind(this),
         800
@@ -85,7 +85,7 @@ class NotificationAlert extends React.Component {
       }
     }
     let toggle;
-    if(options.closeButton !== false){
+    if (options.closeButton !== false) {
       toggle = () => this.onDismiss(nNumber, options.place);
     }
     var notification = React.createElement(
@@ -98,9 +98,9 @@ class NotificationAlert extends React.Component {
       },
       options.icon !== undefined
         ? React.createElement("span", {
-            "data-notify": "icon",
-            className: options.icon
-          })
+          "data-notify": "icon",
+          className: options.icon
+        })
         : null,
       React.createElement("span", { "data-notify": "message" }, options.message)
     );
@@ -112,9 +112,9 @@ class NotificationAlert extends React.Component {
     var sNotify = {};
     sNotify["notify" + options.place.toUpperCase()] = notify;
     // aici pui notify[notify.length-1].key
-    if (options.autoDismiss > 0){
+    if (options.autoDismiss > 0) {
       let id = setTimeout(
-        function() {
+        function () {
           this.onDismiss(nNumber, options.place);
         }.bind(this),
         options.autoDismiss * 1000 + (notify.length - 1) * 1000
@@ -132,7 +132,7 @@ class NotificationAlert extends React.Component {
         margin: "0px auto",
         position: "fixed",
         transition: "all 0.5s ease-in-out",
-        zIndex: "1031"
+        zIndex: "2000"
       };
       if (place.indexOf("t") !== -1) {
         style["top"] = "20px";
@@ -167,13 +167,17 @@ class NotificationAlert extends React.Component {
             break;
         }
       }
-      return React.createElement(
-        Col,
-        { xs: 11, sm: 12, style: style },
-        this.state["notify" + place.toUpperCase()].map((prop, key) => {
-          return prop;
-        })
-      );
+      return this.state["notify" + place.toUpperCase()].map((prop, key) => {
+        return (React.createElement(
+          Row,
+          { style: style, className: "notification-alert" },
+          React.createElement(
+            Col,
+            { xs: 11, sm: 12 },
+            prop,
+          )
+        ))
+      });
     }
   }
   render() {
